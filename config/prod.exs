2bcea33edb5a67886e982ffa997059f2d189ea4e
Mikko Ahlroth <mikko.ahlroth@gmail.com>
2016-05-28 13:15:02 +0300

    Deployment configs

--- config/prod.exs
+++ config/prod.exs
@@ -17,7 +17,7 @@ config :code_stats, CodeStats.Endpoint,
   cache_static_manifest: "priv/static/manifest.json"
 
 # Do not print debug messages in production
-config :logger, level: :info
+config :logger, level: :error
 
 # ## SSL Support
 #
