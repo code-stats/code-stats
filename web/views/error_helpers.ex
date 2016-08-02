30c89fb871591c476cdacb39ae9b7ecdebf1e102
Mikko Ahlroth <mikko.ahlroth@gmail.com>
2016-08-03 00:01:09 +0300

    Fix references to MyApp in error helpers
    Fixes #17

--- web/views/error_helpers.ex
+++ web/views/error_helpers.ex
@@ -32,9 +32,9 @@ defmodule CodeStats.ErrorHelpers do
     #     dgettext "errors", "is invalid"
     #
     if count = opts[:count] do
-      Gettext.dngettext(MyApp.Gettext, "errors", msg, msg, count, opts)
+      Gettext.dngettext(CodeStats.Gettext, "errors", msg, msg, count, opts)
     else
-      Gettext.dgettext(MyApp.Gettext, "errors", msg, opts)
+      Gettext.dgettext(CodeStats.Gettext, "errors", msg, opts)
     end
   end
 
