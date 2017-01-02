313b7c2d10d911ce2f920b120a61d03d8016bf96
Mikko Ahlroth <mikko.ahlroth@gmail.com>
2017-01-02 22:14:55 +0200

    Add tool for unaliasing language

--- lib/code_stats/admin_commands.ex
+++ lib/code_stats/admin_commands.ex
@@ -20,6 +20,7 @@ defmodule CodeStats.AdminCommands do
 
   Moves all XP to the new language and update users' caches.
   """
+  @spec alias_language(String.t, String.t) :: nil
   def alias_language(lang_name, target_name) do
     {:ok, language} = Language.get_or_create(lang_name)
     {:ok, target} = Language.get_or_create(target_name)
@@ -75,6 +76,42 @@ defmodule CodeStats.AdminCommands do
         ]
     )
     |> Repo.update_all([])
+
+    # Update frontpage language caches
+    CodeStats.CacheService.refresh_total_language_xp()
+  end
+
+  @doc """
+  Remove language's alias status.
+
+  All aliased XP will be moved back to this language.
+  """
+  @spec unalias_language(String.t) :: nil
+  def unalias_language(lang_name) do
+    {:ok, language} = Language.get_or_create(lang_name)
+
+    if language.alias_of_id != nil do
+      raise "Cannot unalias a language that is not an alias!"
+    end
+
+    # Remove language alias attribute
+    language
+    |> Changeset.change()
+    |> Changeset.put_change(:alias_of_id, nil)
+    |> Repo.update!()
+
+    # Set all XP that was originally of this language back to this language
+    from(x in XP, where: x.original_language_id == ^language.id)
+    |> Repo.update_all(set: [language_id: language.id])
+
+    id2str = &Integer.to_string/1
+
+    # Update caches for all users (since there's no easy way to separate language points)
+    (from u in User, select: u)
+    |> Repo.all()
+    |> Enum.each(
+      fn user -> User.update_cached_xps(user, true) end
+    )
 
     # Update frontpage language caches
     CodeStats.CacheService.refresh_total_language_xp()
