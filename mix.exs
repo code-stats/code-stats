8d7d836cff23d1d2175a51f2539d907ba508e76a
Mikko Ahlroth <mikko.ahlroth@gmail.com>
2017-10-01 22:23:22 +0300

    Bump version to 1.8.5

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule CodeStats.Mixfile do
 
   def project do
     [app: :code_stats,
-     version: "1.8.4",
+     version: "1.8.5",
      elixir: "~> 1.5",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
