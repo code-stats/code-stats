829cec23ae366e98586cef5060e6371bf4f29fce
Mikko Ahlroth <mikko.ahlroth@gmail.com>
2017-10-10 21:19:02 +0300

    Implement machines page

--- web/static/frontend/js/machine_page.js
+++ web/static/frontend/js/machine_page.js
@@ -11,6 +11,21 @@ function machine_page() {
         return confirm('Are you sure you wish to delete this machine? You will lose all XP associated with it.');
       };
     }
+
+    const copy_buttons = document.getElementsByClassName('copy-button');
+
+    for (const button of copy_buttons) {
+      button.onclick = () => {
+        button.parentElement.getElementsByClassName('api-key')[0].select();
+        try {
+          document.execCommand('copy');
+          button.textContent = 'Copied!';
+        }
+        catch (e) {
+          button.textContent = 'Error';
+        }
+      };
+    }
   });
 }
 
